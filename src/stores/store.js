88b0ba42e95de2115ef38cc6e08200f42a842b08
F6322473 Lucas Henrique Martins Silva <lucashenrique@bb.com.br>
2022-01-20 15:15:43 -0300

    trabalhando no redux toolkit

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,7 +1,24 @@
 import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 import reducers from '../reducers';
 
+const slice = createSlice({
+    name: 'todos',
+    initialState: {},
+    reducers: {
+        addTodo: (state) => { return state },
+        remoteTodo: (state) => { return state },
+        toggleTodo: (state) => { return state },
+    }
+})
 
-const store = createStore(reducers);
+
+const store = configureStore({
+    reducer: {
+        todos: slice.reducer
+    }
+})
+
+// const store = createStore(reducers);
 
 export default store;
